83abe4547606edd33f082c3d15d4d03ef3877db8
Rohitchaudhary13 <71605903+Rohitchaudhary13@users.noreply.github.com>
2021-04-13 02:18:16 +0530

    Add files via upload

---
+++ scriipt.js
@@ -0,0 +1,16 @@
+
+// function containing argument
+function myFunction(x) {
+	// it toggles
+  x.classList.toggle("change");
+   var x = document.getElementById("dropdown");
+  if (x.style.display ==="block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// function myFunction() {
+ 
+// }
